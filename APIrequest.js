3cf6afb9058e4d6729e524dd5499a4756c143750
Aashish Sinha <asinha1@andrew.cmu.edu>
2013-09-22 02:35:05 +0000

    First workable version

--- APIrequest.js
+++ APIrequest.js
@@ -1,5 +1,23 @@
 var request = require("request");
 var http = require("http");
+var mongoose = require("mongoose");
+
+var mongoose = require('mongoose');
+mongoose.connect('ec2-54-200-60-55.us-west-2.compute.amazonaws.com/news');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+   console.log("Connected");
+});
+
+var newsSchema = mongoose.Schema({
+        link : String,
+        bias : Number,
+        lean : String
+})
+var summary = mongoose.model('Summary',newsSchema);
+
 var llist = [];
 var rlist = []; 
 var JSON_extractor = function(list, url) {
@@ -17,7 +35,8 @@ var JSON_extractor = function(list, url) {
     } , function (response) {
             // If the API sent an error msg, ignore keyword
     console.log("Got the API");
-    if(response.status === 'ERROR') {}
+    if(response.status === 'ERROR') 
+    {console.error.bind(console, 'connection error:');}
     else
     {
     //Create object with keyword, score
@@ -40,8 +59,47 @@ var JSON_extractor = function(list, url) {
   return obj;
 }
 
+var get_total_positivity = function(list) {
+  var pos = 0;
+  var neg = 0;
+  for (i=0; i < list.length;i++)
+  {
+    if (list[i].word === "positive") 
+    {
+      pos += score;
+    }
+    if (list[i].word === "negative")
+    {
+      neg += Math.abs(list[i].score);
+    }
+  }
+  return [pos,neg]; 
+}
+
 var extract_feels = function(url) {
   var liberal_feels = JSON_extractor(llist,url);
   var conservative_feels = JSON_extractor(rlist,url);
-
+  var lib_scores = get_total_positivity(liberal_feels);
+  var con_scores = get_total_positivity(conservative_feels);
+  
+  var lbias = Math.abs(lib_scores[0] - lib_scores[1]);
+  var rbias = Math.abs(con_scores[0] - con_scores[1]);
+  var netbias = parseInt(Math.abs(lbias - rbias));
+  if(lbias > rbias)
+  {
+    var lib_sum = new summary({link : url, bias : netbias, lean : "Liberal"});
+    lib_sum.save(function (err,lib_sum) {
+      if (err) //TODO
+        {}
+    });
+  }
+  else
+  {
+    var lib_sum = new summary({link : url, bias : netbias, lean : "Conservative"});
+    lib_sum.save(function (err,lib_sum) {
+      if (err) //TODO
+      {}
+    });
+ 
+  }
 }
